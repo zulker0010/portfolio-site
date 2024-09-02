4cade303cf0316eabb0bd50c7ade350e3f3c83c8
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-09-02 17:55:20 +0600

    added https protocol in astron.config file

--- landing-page/src/components/Bio.tsx
+++ landing-page/src/components/Bio.tsx
@@ -6,10 +6,10 @@ const Bio: React.FC = () => {
    <section className="flex flex-col text-wrap">
         <img 
             src="https://drive.google.com/uc?export=view&id=1mrh0LdDvF6RQZ5je3oMjJdLc9YruXRqW"
-            className="w-40 h-40 md:w-44 md:h-44 rounded-full border mb-4 md:mb-0 md:mr-8"
+            className="w-40 h-40 md:w-56 md:h-56 rounded-full border mb-4 md:mb-0 md:mr-8"
       />
       <div>
-          <h2 className="text-xl p-4 text-left font-mono">Hello! I'm Zulker Yusuf.</h2>
+          <h2 className="text-xl p-6 text-left font-mono">Hello! I'm Zulker Yusuf.</h2>
           <p className="text-gray-950 mt-4"></p>
        </div>
    </section>
