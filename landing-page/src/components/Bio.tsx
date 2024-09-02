db4555295f807f8982aa3a20b24b16ff6f71a01a
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-09-02 17:27:11 +0600

    implemented bio component on astro file, failed loading image

--- landing-page/src/components/Bio.tsx
+++ landing-page/src/components/Bio.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
+
 const Bio: React.FC = () => {
  return(
-   <section className="flex flex-col font-mono text-wrap text-center">
+   <section className="flex flex-col text-wrap">
         <img 
-            src="https://drive.google.com/uc?export=view&id=1iQa1_a1wUCCX8z7jnhbIgpRY9pwuprrr" 
-            alt="My BIO Image" 
-            className="w-32 h-32 md:w-48 md:h-48 rounded-full"
+            src="https://drive.google.com/uc?export=view&id=1mrh0LdDvF6RQZ5je3oMjJdLc9YruXRqW"
+            className="w-40 h-40 md:w-44 md:h-44 rounded-full border mb-4 md:mb-0 md:mr-8"
       />
       <div>
-          <h2 className="text-2xl">Hello! I'm Zulker Yusuf.</h2>
+          <h2 className="text-xl p-4 text-left font-mono">Hello! I'm Zulker Yusuf.</h2>
           <p className="text-gray-950 mt-4"></p>
        </div>
    </section>
--- landing-page/src/components/header.tsx
+++ landing-page/src/components/header.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
  return (
   <header className="bg-white shadow-md py-4">
-    <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-mono">Hello, I'm Zulker Yusuf</h1>
+    <nav className="container mx-auto flex justify-between items-center">       
         <ul className="flex space-x-4">
            <li><a href="/" className="text-gray-500">Home</a></li>
            <li><a href="/about" className="text-gray-500">About</a></li>
