6b02b4d51f219de2c8b7cf344f2a865ca9077528
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-09-13 10:04:52 +0600

    add functionality to tabBar

--- landing-page/src/components/TabBar.tsx
+++ landing-page/src/components/TabBar.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import {Button} from "@/components/ui/button"
 
-const TabBar: React.FC = () => {
-  return(
-  <section className="object-right-top">
-    <Tabs defaultValue="Worth the flex" className="object-right-top">
+type TabType = 'Worth the flex' | 'Essays' | 'Wins' | 'Ls' | 'Contact'
+
+const TabBar:React.FC = () => {
+  const [activeTab, setActiveTab] = useState("Essays")
+
+const onPress = () => {
+  console.log("Hello World")
+}
+
+  return(  <>
+    
+    <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
       {/* Tab List */}
-      <TabsList className="flex w-fit justify-center font-mono">
+      <TabsList className="flex w-fit justify-center border-b-2 font-mono">
           <TabsTrigger value="Worth the flex" className="px-4 py-2 text-sm font-medium">
           Worth the flex
+          
           </TabsTrigger>
           <TabsTrigger value="Essays" className="px-4 py-2 text-sm font-medium">
             Essays
@@ -32,13 +42,27 @@ const TabBar: React.FC = () => {
       </TabsContent>
       <TabsContent value="Essays">
         <div className="py-4">
-          <h2 className="text-lg font-mono font-semibold text">Things I think about</h2>
+          <h2 className="text-2xl font-mono font-semibold text">Things I think about</h2>
+        </div>
+      </TabsContent>
+      <TabsContent value="Wins">
+        <div className="py-4">
+          <h2 className="text-2xl font-mono font-semibold text">A few good stories</h2>
+        </div>
+      </TabsContent>
+      <TabsContent value="Ls">
+        <div className="py-4">
+          <h2 className="text-2xl font-mono font-semibold text">A LOT of learning curves</h2>
+        </div>
+      </TabsContent>
+      <TabsContent value="Contact">
+        <div className="py-4">
+          <h2 className="text-2xl font-mono font-semibold text">Ways to reach me</h2>
         </div>
       </TabsContent>
     
-
-    </Tabs>
-  </section>
+</Tabs>
+</>
   )
 }
 
