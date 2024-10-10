a0487b12fd2707deba3066a4b64e8e24cd49009c
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-10-10 14:21:05 +0600

    fixed image to project-card mapping

--- landing-page/src/components/Card.tsx
+++ landing-page/src/components/Card.tsx
@@ -2,30 +2,26 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, CardBody, CardImg, CardTitle, CardText} from 'reactstrap';
 
-interface ProjectCardProps{
-  title: string;
-  description: string;
-}
 
-const ProjectCard: React.FC<ProjectCardProps>=({title,description}) => {
+const ProjectCard: React.FC = () => {
  return(
   <>
   <Card className="my-2 rounded-none">
     <CardImg className="rounded-none"
-      src='src/assets/mentors_mobile_app.jpg'
+      src= 'src/assets/netflix_dashboard.jpg'
       alt="nothing"
       style={{
         height: "100%"      
       }}
       top
       width="100%"
-    />
+    ></CardImg>
     <CardBody className='font-mono font-bold'>
       <CardTitle tag="h5">
-        {title}
+      Netflix Dashboard in Tableau
       </CardTitle>
       <CardText className="font-medium">
-        {description}
+      Statisticial visualization of everything the world saw in Netflix
       </CardText>
       <CardText>
         <small className="text-muted">
@@ -34,6 +30,55 @@ const ProjectCard: React.FC<ProjectCardProps>=({title,description}) => {
       </CardText>
     </CardBody>
   </Card>
+  <Card className="my-2 rounded-none">
+    <CardImg className="rounded-none"
+      src= 'src/assets/mentors_mobile_app.jpg'
+      alt="nothing"
+      style={{
+        height: "100%"      
+      }}
+      top
+      width="100%"
+    ></CardImg>
+    <CardBody className='font-mono font-bold'>
+      <CardTitle tag="h5">
+      Mentors mobile app v1.0'
+      </CardTitle>
+      <CardText className="font-medium">
+      A cross platform mobile app built for the education & student immigration company Mentors.
+      </CardText>
+      <CardText>
+        <small className="text-muted">
+          Last updated 3 mins ago
+        </small>
+      </CardText>
+    </CardBody>
+  </Card>
+  <Card className="my-2 rounded-none">
+    <CardImg className="rounded-none"
+      src= 'src/assets/divorce_probability.jpg'
+      alt="nothing"
+      style={{
+        height: "100%"      
+      }}
+      top
+      width="100%"
+    ></CardImg>
+    <CardBody className='font-mono font-bold'>
+      <CardTitle tag="h5">
+      Divorce probability correlation
+      </CardTitle>
+      <CardText className="font-medium">
+      A py script that analyzes the divorce probability of couples based on 50 different factors. 
+      </CardText>
+      <CardText>
+        <small className="text-muted">
+          Last updated 3 mins ago
+        </small>
+      </CardText>
+    </CardBody>
+  </Card>
+
 </>
  )
 };
