a530009fb9ee767a0206d13fdea76c84b36d507e
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-10-10 18:24:22 +0600

    wrote deployment script yml

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -7,6 +7,8 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https/zulker0010.github.io',
+  base: '/zulkeryusuf.github.io',
   integrations: [tailwind({
     applyBaseStyles: false, //change to False after shadecn config
   }), react(), vue()],
